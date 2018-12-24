3b16a14c373e9988d4a9dff598e0ecd400bab303
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-12-24 08:31:09 -0800

    Added basic setup for querying yaml files.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,4 +2,17 @@ module.exports = {
   siteMetadata: {
     title: "CodeByWomen",
   },
+  plugins: [
+    'gatsby-transformer-yaml',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-image',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './content/',
+      }
+    },
+  ]
 }
