81bc59414888a78244d5fd4686c0d0c85573ead5
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-12-24 15:48:47 -0800

    Increased font sizing.

--- src/common/components/blog-card.js
+++ src/common/components/blog-card.js
@@ -8,9 +8,9 @@ import '../styles/custom.tachyons.css';
 
 export default props => (
   <Card>
-    <Link to={props.slug} className="db f4 sans-serif mv1 navy no-underline">{props.title}</Link>
+    <Link to={props.slug} className="db f3 sans-serif mv1 navy no-underline">{props.title}</Link>
     <span className="db f5 sans-serif gray">by {props.author}</span>
-    <p className="db f5 sans-serif lh-copy">{props.description}</p>
+    <p className="db f4 sans-serif lh-copy">{props.description}</p>
     <div className="flex flex-wrap justify-start">
       {props.tags && props.tags.map(tag => (
         <Tag>{tag}</Tag>
