9a4d4f422c11143b4959f1e1a2b4073c641f45e2
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-12-24 09:15:33 -0800

    Adjusted type colors.

--- src/common/components/blog-card.js
+++ src/common/components/blog-card.js
@@ -4,9 +4,9 @@ import 'tachyons';
 
 export default props => (
   <div className="bg-white bl bw2 b--blue pa3 shadow-5 w-100">
-    <span className="db f4 sans-serif mv1">{props.title}</span>
+    <span className="db f4 sans-serif mv1 navy">{props.title}</span>
     <span className="db f5 sans-serif gray">by {props.author}</span>
-    <p className="db f5 sans-serif mid-gray lh-copy">{props.description}</p>
+    <p className="db f5 sans-serif lh-copy">{props.description}</p>
     <div className="flex flex-wrap justify-start">
       {props.tags && props.tags.map(tag => (
         <span className="pa1 mr1 bg-lightest-blue navy sans-serif ttc pointer">{tag}</span>
