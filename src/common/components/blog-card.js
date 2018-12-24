9b1383aaa908ad61b73ce3143934fb2046919be5
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-12-24 10:51:08 -0800

    Removed Underline.

--- src/common/components/blog-card.js
+++ src/common/components/blog-card.js
@@ -6,7 +6,7 @@ import '../styles/custom.tachyons.css';
 
 export default props => (
   <div className="bg-white bl bw2 br2 b--blue pa3 shadow-5 w-100 mv2">
-    <Link to={props.slug} className="db f4 sans-serif mv1 navy">{props.title}</Link>
+    <Link to={props.slug} className="db f4 sans-serif mv1 navy no-underline">{props.title}</Link>
     <span className="db f5 sans-serif gray">by {props.author}</span>
     <p className="db f5 sans-serif lh-copy">{props.description}</p>
     <div className="flex flex-wrap justify-start">
