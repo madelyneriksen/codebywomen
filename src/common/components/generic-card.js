e5673c6f960c93c1c9afeecc555f5f70876bb46f
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-12-25 07:07:50 -0800

    Changed card bordering and shadows.

--- src/common/components/generic-card.js
+++ src/common/components/generic-card.js
@@ -4,7 +4,7 @@ import '../styles/custom.tachyons.css';
 
 
 export default props => (
-  <div className="bg-white bl bw2 br2 b--blue pa3 shadow-5 w-100 mv3">
+  <div className="bg-white br2 b--blue pa3 shadow-4 w-100 mv3">
     {props.children}
   </div>
 )
