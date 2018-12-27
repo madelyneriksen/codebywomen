a4c5c2678d71693c340a66d6cca32451ddc6910d
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-12-27 06:40:05 -0800

    Modified navbar.

--- src/common/components/navbar.js
+++ src/common/components/navbar.js
@@ -7,7 +7,7 @@ import '../styles/custom.tachyons.css';
 export const PureNavbar = props => {
   const data = props.data;
   return (
-    <div className="bg-white h3 flex items-center pa2">
+    <div className="bg-white h3 flex items-center ph2 top-0 z-999 shadow-1" style={{position: "sticky"}}>
       <Link className="f3 accent-font no-underline dark-gray" to="/">{data.site.siteMetadata.title}</Link>
     </div>
   )
