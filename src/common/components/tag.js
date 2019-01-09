6fd904be69f9a0d896bb23797a2940015364b6a3
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-01-09 06:10:35 -0800

    Removed tag component.

--- src/common/components/tag.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import 'tachyons';
-import '../styles/custom.tachyons.css'
-
-
-export default props => (
-  <span className="pv1 ph2 mr1 mv1 bg-lightest-blue navy sans-serif ttc pointer br-pill dib">{props.children}</span>
-)
