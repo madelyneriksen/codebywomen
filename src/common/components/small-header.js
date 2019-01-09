a6b4e96b7d0290e67988661bace4401848c9c8b1
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-01-09 06:23:31 -0800

    Removed unused components.

--- src/common/components/small-header.js
+++
@@ -1,9 +0,0 @@
-// Small Header for details cards.
-import React from 'react';
-import 'tachyons';
-import '../styles/custom.tachyons.css';
-
-
-export default props => (
-  <h2 className="accent-font ma0 navy">{props.children}</h2>
-)
