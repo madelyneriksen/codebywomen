a67ea12fcca16d341d10e5deb811cefe1fa5377e
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-01-09 05:48:40 -0800

    Updated card layouts.

--- src/common/components/blog-card.js
+++ src/common/components/blog-card.js
@@ -10,16 +10,16 @@ import '../styles/custom.tachyons.css';
 export default props => (
   <Card className="flex flex-column">
     {props.image &&
-        <Link to={props.slug} className="db mb1">
-          <Img
-            fluid={props.image.childImageSharp.fluid}
-            alt={`Screenshot of the blog ${props.title}`}
-            className="h5" />
-        </Link>
+      <Link to={props.slug} className="db mb1">
+        <Img
+          fluid={props.image.childImageSharp.fluid}
+          alt={`Screenshot of the blog ${props.title}`}
+          className="h5" />
+      </Link>
     }
-    <div className="pa3 flex flex-column justify-between h-100">
+    <div className="pv3 flex flex-column justify-between h-100">
       <div>
-        <Link to={props.slug} className="db f3 sans-serif mb1 navy no-underline">{props.title}</Link>
+        <Link to={props.slug} className="db f3 sans-serif mb1 dark-gray no-underline">{props.title}</Link>
         <div className="flex flex-wrap justify-start">
         {props.tags && props.tags.map(tag => (
           <Tag>{tag}</Tag>
@@ -27,15 +27,14 @@ export default props => (
         </div>
       </div>
       <div>
-        <p className="db f4 sans-serif lh-copy">{props.description}</p>
+        <p className="db f5 sans-serif lh-copy">{props.description}</p>
       </div>
       <div className="flex justify-start">
-        <Link to={props.slug} className="mr4 sans-serif blue f4 no-underline">View Details</Link>
         <a
           href={props.url}
           target="_blank"
           rel="noopener noreferrer"
-          className="sans-serif blue f4 no-underline">Visit Site</a>
+          className="sans-serif near-white bg-dark-gray b ttu w-100 no-underline pv3 tc">Visit Site</a>
       </div>
     </div>
   </Card>
