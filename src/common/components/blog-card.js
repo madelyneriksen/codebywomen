499fe58a5a3a75342e29ca257eac16acbd6ef8f7
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-01-09 06:15:21 -0800

    Removed slug references.

--- src/common/components/blog-card.js
+++ src/common/components/blog-card.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import { NoSpacingCard as Card } from './generic-card';
 import Img from 'gatsby-image';
 import 'tachyons';
@@ -9,16 +8,16 @@ import '../styles/custom.tachyons.css';
 export default props => (
   <Card className="flex flex-column">
     {props.image &&
-      <Link to={props.slug} className="db mb1">
+      <div>
         <Img
           fluid={props.image.childImageSharp.fluid}
           alt={`Screenshot of the blog ${props.title}`}
           className="h5" />
-      </Link>
+      </div>
     }
-    <div className="pv3 flex flex-column justify-between h-100">
+    <div className="pv2 flex flex-column justify-between h-100">
       <div>
-        <Link to={props.slug} className="db f3 sans-serif mb1 dark-gray no-underline">{props.title}</Link>
+        <h3 className="fw3 db f3 sans-serif mb1 dark-gray no-underline">{props.title}</h3>
         <a href={props.url} target="_blank" rel="noopener noreferrer" className="sans-serif gray">{props.url}</a>
         <p className="db f5 sans-serif lh-copy">{props.description}</p>
       </div>
