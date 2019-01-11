668be7c579d087150efbc49d4cc1425a7669a914
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-01-11 06:34:25 -0800

    Updated icon list styling.

--- src/common/components/icon-list.js
+++ src/common/components/icon-list.js
@@ -11,21 +11,21 @@ import {
 
 
 export default props => (
-    <div className="flex justify-around items-center f3 pv2">
+    <div className="flex justify-center items-center f3">
       {props.links.github &&
-        <a href={props.links.github} className="dark-gray"><IoLogoGithub /></a>
+        <a href={props.links.github} className="dark-gray ma2"><IoLogoGithub /></a>
       }
       {props.links.twitter &&
-        <a href={props.links.twitter} className="dark-gray"><IoLogoTwitter /></a>
+        <a href={props.links.twitter} className="dark-gray ma2"><IoLogoTwitter /></a>
       }
       {props.links.youtube &&
-        <a href={props.links.youtube} className="dark-gray"><IoLogoYoutube /></a>
+        <a href={props.links.youtube} className="dark-gray ma2"><IoLogoYoutube /></a>
       }
       {props.links.pinterest &&
-        <a href={props.links.pinterest} className="dark-gray"><IoLogoPinterest /></a>
+        <a href={props.links.pinterest} className="dark-gray ma2"><IoLogoPinterest /></a>
       }
       {props.links.instagram &&
-        <a href={props.links.instagram} className="dark-gray"><IoLogoInstagram /></a>
+        <a href={props.links.instagram} className="dark-gray ma2"><IoLogoInstagram /></a>
       }
     </div>
 )
