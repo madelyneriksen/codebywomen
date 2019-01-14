18c0a05aabf05b328e2bd96736b0372002e55cc0
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-01-14 06:02:48 -0800

    Added contributing link.

--- src/common/components/navbar.js
+++ src/common/components/navbar.js
@@ -14,7 +14,7 @@ export default Navbar => (
     <div className="bg-near-black pv4 flex items-center ph3 z-999 top-0" style={{position: "sticky"}}>
       <Link className="washed-blue tracked ttu sans-serif no-underline db" to="/about">About &bull;&nbsp;</Link>
       <Link className="washed-blue tracked ttu sans-serif no-underline db" to="/contact">Contact &bull;&nbsp;</Link>
-      <Link className="washed-blue tracked ttu sans-serif no-underline db" to="/submit">Submit</Link>
+      <a className="washed-blue tracked ttu sans-serif no-underline db" href="https://github.com/madelyneriksen/codebywomen">Contribute</a>
     </div>
   </React.Fragment>
 )
