c33a3875bebe8a4433d3270e7685ec34de5f7678
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-01-15 05:28:43 -0800

    Swapped typography sizes.

--- src/contact/messages.js
+++ src/contact/messages.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'tachyons';
 
 
-const messageClasses = "sans-serif pa3 ba db br3 f4 ";
+const messageClasses = "sans-serif pa3 ba db br3 f5 ";
 const successClasses = "bg-washed-green b--green dark-green";
 const errorClasses = "bg-washed-red b--red dark-red";
 
