a6e9b6bc366263951258db8b8784b7a9d6437614
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-01-15 06:20:30 -0800

    Improved navbar UX.

--- src/common/components/navbar.js
+++ src/common/components/navbar.js
@@ -4,17 +4,22 @@ import 'tachyons';
 import '../styles/custom.tachyons.css';
 
 
+const linkStyles = "near-white hover-mid-gray tracked-ns ttu sans-serif no-underline db mr2 mr4-ns";
+const barStyles = "bg-near-black pv4 flex items-center justify-center justify-start-ns php2 ph3-ns z-999";
+
+
 export default Navbar => (
   <React.Fragment>
-    <div className="bg-near-black pv4 flex items-center ph3 z-999">
+    <div className={barStyles}>
       <Link
         className="f2 f1-l fw8 ttu sans-serif no-underline washed-blue tracked-tight"
         to="/">{"{"} Code<span className="fw3"> By </span>Women {"}"}</Link>
     </div>
-    <div className="bg-near-black pv4 flex items-center ph3 z-999 top-0" style={{position: "sticky"}}>
-      <Link className="washed-blue tracked ttu sans-serif no-underline db" to="/about">About &bull;&nbsp;</Link>
-      <Link className="washed-blue tracked ttu sans-serif no-underline db" to="/contact">Contact &bull;&nbsp;</Link>
-      <a className="washed-blue tracked ttu sans-serif no-underline db" href="https://github.com/madelyneriksen/codebywomen">Contribute</a>
+    <div className={barStyles + " top-0"} style={{position: "sticky"}}>
+      <Link className={linkStyles} to="/">Home</Link>
+      <Link className={linkStyles} to="/about">About</Link>
+      <Link className={linkStyles} to="/contact">Contact</Link>
+      <a className={linkStyles} href="https://github.com/madelyneriksen/codebywomen">Contribute</a>
     </div>
   </React.Fragment>
 )
