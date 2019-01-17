d05bac9380edc41a16efb70cd0a85dfcf1c7ebe7
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-01-17 06:03:47 -0800

    Fixed navbar styles.

--- src/common/components/navbar.js
+++ src/common/components/navbar.js
@@ -5,17 +5,17 @@ import '../styles/custom.tachyons.css';
 
 
 const linkStyles = "near-white hover-mid-gray tracked-ns ttu sans-serif no-underline db mr2 mr4-ns";
-const barStyles = "bg-near-black pv4 flex items-center justify-center justify-start-ns php2 ph3-ns z-999";
+const barStyles = "bg-near-black flex items-center justify-center justify-start-ns ph2 ph3-ns z-999";
 
 
 export default Navbar => (
   <React.Fragment>
-    <div className={barStyles}>
+    <div className={barStyles + " pv3"}>
       <Link
         className="f2 f1-l fw8 ttu sans-serif no-underline washed-blue tracked-tight"
         to="/">{"{"} Code<span className="fw3"> By </span>Women {"}"}</Link>
     </div>
-    <div className={barStyles + " top-0"} style={{position: "sticky"}}>
+    <div className={barStyles + " pv4 top-0"} style={{position: "sticky"}}>
       <Link className={linkStyles} to="/">Home</Link>
       <Link className={linkStyles} to="/about">About</Link>
       <Link className={linkStyles} to="/contact">Contact</Link>
