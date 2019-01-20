2a42c547639352263ab649afa3c0e3f8bd1503b6
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-01-20 13:04:07 -0800

    Updated footer text.

--- src/common/components/footer.js
+++ src/common/components/footer.js
@@ -9,7 +9,7 @@ export default () => (
     <span className="db w-100 tc self-center sans-serif">Built with React and Gatsby.</span>
     <span
       className="db w-100 tc self-center sans-serif"
-    >See the source code on <a
+    >Grab a copy of the source code on <a
         className="dark-gray"
         href="https://github.com/madelyneriksen/codebywomen">Github.</a></span>
     <span className="db w-100 tc self-end sans-serif bg-moon-gray pa1">Copyright 2019 Madelyn Eriksen</span>
